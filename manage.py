361ec8d6fa4e48e90459c1017388cae630c615cf
Oliver Zander <oliver.zander@gmail.com>
2014-10-22 23:15:43 +0200

    set environ path

--- manage.py
+++ manage.py
@@ -4,8 +4,7 @@ import sys
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'budgetbutler.settings.production')
-    sys.path.append('/app/vendor/node/bin/')
-    sys.path.append('/app/node_modules/.bin/')
+    os.environ['PATH'] += os.pathsep + os.path.join('app', 'vendor', 'node', 'bin')
 
     from django.core.management import execute_from_command_line
 
