f7063f612a68e1c6fb3c88be03bea681049f06c3
Oliver Zander <oliver.zander@gmail.com>
2014-10-22 23:44:21 +0200

    removed fix version

--- manage.py
+++ manage.py
@@ -4,7 +4,6 @@ import sys
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'budgetbutler.settings.production')
-    os.environ['PATH'] += os.pathsep + os.path.join('app', 'vendor', 'node', 'bin')
 
     from django.core.management import execute_from_command_line
 
